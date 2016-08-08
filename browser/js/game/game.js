a90d85fcfb1fdba542f34cf7c4e4d945050eacd7
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-08-07 23:48:25 -0400

    donedonedone

--- browser/js/game/game.js
+++ browser/js/game/game.js
@@ -10,6 +10,8 @@ app.config(function ($stateProvider) {
 app.controller('GameController', function ($scope,$stateParams,GameFactory) {
     var gameWindow=gameframe.contentWindow;
     if($stateParams.hash){
+        $scope.linkText='Link to share:';
+        $scope.link=window.location.href||'https://re-spect.herokuapp.com/game/'+$stateParams.hash;
         GameFactory.getGame($stateParams.hash)
         .then(function(game){
             var gameData=eval('(' + game.data+ ')')
